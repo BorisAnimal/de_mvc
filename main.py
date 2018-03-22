ca90c5da50d0e6ee1cecb0e6bb6abb0ae54bae4b
BorisAnimal <iguru98@yandex.ru>
2018-03-22 04:33:31 -0400

    main

--- main.py
+++ main.py
@@ -4,9 +4,13 @@
 # TODO: fix chunks in funciton
 
 import random
+import sys
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+
+from filter import Selector
+from controller import *
 
 from PyQt4 import QtGui, QtCore
 
@@ -52,36 +56,92 @@ class MyWindow(QtGui.QWidget):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
 
-        self.pushButtonPlot = QtGui.QPushButton(self)
-        self.pushButtonPlot.setText("Plot")
-        self.pushButtonPlot.clicked.connect(self.on_pushButtonPlot_clicked)
+        self.controller = Controller()
 
-        self.matplotlibWidget = MatplotlibWidget(self)
+        self.solutionGraphWidget = MatplotlibWidget(self)
+        self.errorGraphWidget = MatplotlibWidget(self)
 
-        self.layoutVertical = QtGui.QVBoxLayout(self)
-        self.layoutVertical.addWidget(self.pushButtonPlot)
-        self.layoutVertical.addWidget(self.matplotlibWidget)
+        # Main layout (horizontal)
+        self.hbox = QtGui.QHBoxLayout(self)
+        self.hbox.addStretch(1)
+        # Left control panel layout (vertical)
+        self.control_box = QtGui.QVBoxLayout(self)
+        self.hbox.addLayout(self.control_box)
+
+        # init views
+        self.x0Edit = QtGui.QLineEdit()
+        self.y0Edit = QtGui.QLineEdit()
+        self.XEdit = QtGui.QLineEdit()
+        self.notificationsEdit = QtGui.QTextEdit()
+        self.notificationsEdit.setEnabled(False)
+        self.NEdit = QtGui.QLineEdit()
+        self.exactButton = QtGui.QPushButton("Exact")
+        self.eulerButton = QtGui.QPushButton("Euler")
+        self.eulerImpButton = QtGui.QPushButton("ImprovedEuler")
+        self.rkButton = QtGui.QPushButton("Runge Kutta")
+
+        # Control panel composition
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(18)
+
+        grid.addWidget(QtGui.QLabel('Initial'), 0, 0)
+        grid.addWidget(QtGui.QLabel('Conditions'), 0, 1)
+
+        grid.addWidget(QtGui.QLabel('x0'), 2, 0)
+        grid.addWidget(self.x0Edit, 2, 1)
+
+        grid.addWidget(QtGui.QLabel('y0'), 3, 0)
+        grid.addWidget(self.y0Edit, 3, 1)
+
+        grid.addWidget(QtGui.QLabel('X'), 4, 0)
+        grid.addWidget(self.XEdit, 4, 1)
+
+        grid.addWidget(QtGui.QLabel('N'), 5, 0)
+        grid.addWidget(self.NEdit, 5, 1)
+
+        grid.addWidget(self.exactButton, 6, 0, 1, 2)  # int fromRow, int fromColumn, int rowSpan, int columnSpan
+        grid.addWidget(self.eulerButton, 7, 0, 1, 2)
+        grid.addWidget(self.eulerImpButton, 8, 0, 1, 2)
+        grid.addWidget(self.rkButton, 9, 0, 1, 2)
+
+        grid.addWidget(self.notificationsEdit, 10, 0, 1, 2)
+
+        self.control_box.addLayout(grid)
+
+        # init graph's layout
+        rgrid = QtGui.QGridLayout()
+        rgrid.setSpacing(2)
+        rgrid.addWidget(self.solutionGraphWidget)
+        rgrid.addWidget(self.errorGraphWidget)
+        self.hbox.addLayout(rgrid)
+
+        self.exactButton.clicked.connect(lambda: self.on_algorithmButton_clicked(selector=Selector.EXACT))
+        self.eulerButton.clicked.connect(lambda: self.on_algorithmButton_clicked(selector=Selector.EULER))
+        self.eulerImpButton.clicked.connect(lambda: self.on_algorithmButton_clicked(selector=Selector.IMP_EULER))
+        self.rkButton.clicked.connect(lambda: self.on_algorithmButton_clicked(selector=Selector.RUNGE_KUTTA))
 
         self.threadSample = ThreadSample(self)
         self.threadSample.newSample.connect(self.on_threadSample_newSample)
         self.threadSample.finished.connect(self.on_threadSample_finished)
+        self.update_state()
+
+    def update_state(self):
+        self.solutionGraphWidget.axis.clear()
 
     @QtCore.pyqtSlot()
-    def on_pushButtonPlot_clicked(self):
-        self.samples = 0
-        self.matplotlibWidget.axis.clear()
+    def on_algorithmButton_clicked(self, selector):
+        self.controller.filter.selected_method = selector
+        self.solutionGraphWidget.axis.clear()
         self.threadSample.start()
 
     @QtCore.pyqtSlot(list)
     def on_threadSample_newSample(self, sample):
-        self.matplotlibWidget.axis.plot(sample)
-        self.matplotlibWidget.canvas.draw()
+        self.solutionGraphWidget.axis.plot(sample)
+        self.solutionGraphWidget.canvas.draw()
 
     @QtCore.pyqtSlot()
     def on_threadSample_finished(self):
-        self.samples += 1
-        if self.samples <= 2:
-            self.threadSample.start()
+        self.threadSample.start()
 
 
 if __name__ == "__main__":
@@ -91,7 +151,7 @@ if __name__ == "__main__":
     app.setApplicationName('Differential Equations')
 
     main = MyWindow()
-    main.resize(800, 400)
+    main.resize(1000, 800)
     main.show()
 
     sys.exit(app.exec_())
